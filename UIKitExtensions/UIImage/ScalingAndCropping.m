abc0ffe22f0f79709fff552b586fe7a272a6fc8b
stanislaw <s.pankevich@gmail.com>
2013-09-13 15:42:48 +0300

    UIImage/ScalingAndCropping - minor change

--- UIKitExtensions/UIImage/ScalingAndCropping.m
+++ UIKitExtensions/UIImage/ScalingAndCropping.m
@@ -7,7 +7,8 @@
 
 - (UIImage *)imageByScalingAndCroppingForSize:(CGSize)targetSize {
     UIImage *sourceImage = self;
-    UIImage *newImage = nil;
+    UIImage *newImage;
+
     CGSize imageSize = sourceImage.size;
     CGFloat width = imageSize.width;
     CGFloat height = imageSize.height;
@@ -18,50 +19,43 @@
     CGFloat scaledHeight = targetHeight;
     CGPoint thumbnailPoint = CGPointMake(0.0,0.0);
 
-    if (CGSizeEqualToSize(imageSize, targetSize) == NO)
-    {
+    if (CGSizeEqualToSize(imageSize, targetSize) == NO) {
         CGFloat widthFactor = targetWidth / width;
         CGFloat heightFactor = targetHeight / height;
 
-        if (widthFactor > heightFactor)
-        {
+        if (widthFactor > heightFactor) {
             scaleFactor = widthFactor;
-        }
-        else
-        {
+        } else {
             scaleFactor = heightFactor;
         }
 
         scaledWidth  = width * scaleFactor;
         scaledHeight = height * scaleFactor;
 
-        if (widthFactor > heightFactor)
-        {
+        if (widthFactor > heightFactor) {
             thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
-        }
-        else
-        {
-            if (widthFactor < heightFactor)
-            {
-                thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
-            }
+        } else {
+            thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
         }
     }
 
-    UIGraphicsBeginImageContextWithOptions(targetSize, YES, 0.0);
-    
     CGRect thumbnailRect = CGRectZero;
     thumbnailRect.origin = thumbnailPoint;
     thumbnailRect.size.width  = scaledWidth;
     thumbnailRect.size.height = scaledHeight;
 
-    [sourceImage drawInRect:thumbnailRect];
 
-    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsBeginImageContextWithOptions(targetSize, YES, 0.0);
+
+        [sourceImage drawInRect:thumbnailRect];
+
+        newImage = UIGraphicsGetImageFromCurrentImageContext();
 
     UIGraphicsEndImageContext();
-    
+
+
     return newImage;
 }
 
 @end
+
