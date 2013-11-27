c7ecff26304021665eb7b2f336a2f221d4de5c7b
Stanislaw Pankevich <s.pankevich@gmail.com>
2013-11-28 00:12:01 +0200

    Add UI_NULL_VALUE_TO_BE_CHANGED_LATER

--- UIKitExtensions/UIHelpers.h
+++ UIKitExtensions/UIHelpers.h
@@ -7,6 +7,8 @@
 
 #import <UIKit/UIKit.h>
 
+#define UI_NULL_VALUE_TO_BE_CHANGED_LATER 0
+
 #define UI_DESIGNATED_VALUE(value) value
 #define UI_CONTEXT_VALUE(value) value
 #define UI_CONTEXT_OFFSET(offset) offset
